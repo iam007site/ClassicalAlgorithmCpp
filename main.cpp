99fd5ca96489b7361f9a76d7a9858f516e717b0a
huangshanqi <huangshanqi@baidu.com>
2016-09-16 23:18:05 +0800

    add stack operation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
-
-
+#include <time.h>
+#include <math.h>
+#include <stack>
 using namespace std;
 
 void println(char* data)
@@ -253,10 +254,236 @@ void test_MyBinarySearch()
     cout << myBinarySearch(data, 8, 5);
 }
 
+struct LNode{
+    LNode* next;
+    int value;
+};
+
+// 删除单链表中值为value的节点，输入链表带有一个无效的头节点head
+void deleteLNode(LNode* head, int value)
+{
+    LNode* newHead = new LNode;
+    newHead->value = 0;
+    newHead->next = head;
+
+    LNode* pre = newHead;
+    LNode* next = newHead->next;
+    while(NULL != next)
+    {
+        if(value == next->value){
+            pre->next = next->next;
+            delete next;
+            next = pre->next;
+        }
+        else
+        {
+            pre = next;
+            next = next->next;
+        }
+    }
+}
+
+// 在链表的index位置插入值value
+void insertLNode(LNode *head, int index, int value)
+{
+    LNode *iter = head;
+    for (int i=0;i<index && NULL != iter;i++)
+    {
+        ;
+    }
+    if (NULL != iter)
+    {
+        LNode* node = new LNode;
+        node->value = value;
+        node->next = iter->next;
+        iter->next = node;
+    }
+}
+
+// 反转单链表
+LNode* reverLNode(LNode* head)
+{
+    LNode* newHead = new LNode;
+    newHead->value = 0;
+    newHead->next = NULL;
+
+    LNode* iter = newHead;
+    LNode* temp = NULL;
+    while(NULL != head)
+    {
+        temp = head;
+        head = head->next;
+        temp->next = newHead->next;
+        newHead->next = temp;
+    }
+    return newHead;
+}
+
+void test_reverLNode()
+{
+    LNode* head = new LNode;
+    head->value = 0;
+    head->next = 0;
+    LNode* iter = head;
+    for(int i=1;i<6;i++)
+    {
+        LNode* node = new LNode;
+        node->value = i;
+        node->next = NULL;
+        iter->next = node;
+        iter = iter->next;
+    }
+
+    LNode* start = head;
+    while(start != NULL)
+    {
+        cout << start->value << endl;
+        start = start->next;
+    }
+
+    head = reverLNode(head);
+    while(head != NULL)
+    {
+        cout << head->value << endl;
+        head = head->next;
+    }
+}
+
+// 斐波那契数列
+int fibonacci1(int n)
+{
+    if(n < 0)
+    {
+        return -1;
+    }
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return fibonacci1(n-1) + fibonacci1(n-2);
+}
+
+int fibonacci2(int n)
+{
+    if(n < 0)
+    {
+        return -1;
+    }
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    int n2 = 0;
+    int n1 = 1;
+    int result = 0;
+    for (int i=2;i<=n;i++)
+    {
+        result = n1 + n2;
+        n2 = n1;
+        n1 = result;
+    }
+    return result;
+}
+
+void test_fibonacci()
+{
+    for (int i=0;i<11;i++)
+    {
+        cout << fibonacci1(i) << endl;
+    }
+
+    for (int i=0;i<11;i++)
+    {
+        cout << fibonacci2(i) << endl;
+    }
+}
+
+// 产生[min,max]之间的一个随机整数
+int randomInt(int min,int max)
+{
+    if(min > max)
+    {
+        min = min^max;
+        max = max^min;
+        min = min^max;
+    }
+    return rand() % (max - min + 1) + min;
+}
+
+void test_randomInt()
+{
+    for(int i=0;i<10;i++)
+    {
+        cout << randomInt(i*10,100) << endl;
+    }
+}
+
+// 是否是素数
+bool isPrime(int n)
+{
+    for (int i= 2;i<=(int)sqrt(n);i++)
+    {
+        if(n%i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 打印[2, n]的素数
+void printPrime(int n)
+{
+    for (int i=2;i<=n;i++)
+    {
+        if(isPrime(i))
+        {
+            cout << i << endl;
+        }
+    }
+}
+
+void test_PrintPrime()
+{
+    printPrime(100);
+}
+
+// 用栈来实现队列的FIFO
+int queuePop(stack<int> &stacka)
+{
+    stack<int> stackb;
+    while(!stacka.empty())
+    {
+        stackb.push(stacka.top());
+        stacka.pop();
+    }
+    const int head = stackb.top();
+    stackb.pop();
+    while(!stackb.empty())
+    {
+        stacka.push(stackb.top());
+        stackb.pop();
+    }
+    return head;
+}
+
+void test_queuePop()
+{
+    stack<int> stack1;
+    for (int i=0;i<10;i++)
+    {
+        stack1.push(i);
+    }
+    while(!stack1.empty())
+    {
+        cout<< queuePop(stack1) << endl;
+    }
+}
+
 int main()
 {
     cout << "http://blog.csdn.net/f_r_e_e_x/article/details/50770907" << endl;
-
+    srand((unsigned int)time(NULL));
     // test_mystrcpy();
     // test_mystrcat();
     // test_MyString();
@@ -265,6 +492,11 @@ int main()
     // test_myStringRevers();
     // test_myToUpper();
     // test_MyBinarySearch();
+    // test_reverLNode();
+    // test_fibonacci();
+    // test_randomInt();
+    // test_PrintPrime();
+    test_queuePop();
     system("pause");
     return 0;
 }
